4aa426683514454d2d8bff5dca7f55459e1e2820
bdsipon <sipon.here@gmail.com>
2018-02-24 04:37:24 +1000

    commit

---
+++ Model/ParaRight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class ParaRight
+    {
+        [Key]
+        public int ParaId { get; set; }
+        public string ParaText { get; set; }      
+    }
+
+}
